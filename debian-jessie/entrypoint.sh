bfbd970333a86ef91c799de9984e8f05c7c3e69b
William Dizon <wdchromium@gmail.com>
2016-03-07 14:38:00 -0700

    USER_NAME and USER_PASSWORD included in entrypoint

--- debian-jessie/entrypoint.sh
+++ debian-jessie/entrypoint.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 set -eo pipefail
 
-sh /usr/games/minecraft/generate-sslcert.sh
-
-if [ -z "$MC_PASSWORD" ]; then
-  echo >&2 'You need to specify MC_PASSWORD'
+if [ -z "$USER_PASSWORD" ]; then
+  echo >&2 'You need to specify USER_PASSWORD'
   exit 1
-else
-  echo "mc:$MC_PASSWORD" | chpasswd
 fi
+
+if [ "$USER_NAME" ]; then
+  # username specifically provided, will overwrite 'mc'
+  if [[ "$USER_NAME" =~ [^a-zA-Z0-9] ]]; then
+    echo >&2 'USER_NAME must contain only alphanumerics [a-zA-Z0-9]'
+    exit 1
+  fi
+else
+  echo >&2 'USER_NAME not provided; defaulting to "mc"'
+  USER_NAME=mc
+fi
+
+useradd -Ms /bin/false $USER_NAME
+echo "$USER_NAME:$USER_PASSWORD" | chpasswd
+echo >&2 "Created user: $USER_NAME"
+
+echo >&2 "Generating Self-Signed SSL..."
+sh /usr/games/minecraft/generate-sslcert.sh
 
 exec "$@"
