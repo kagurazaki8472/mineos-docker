01232210c310e305c0bff8d7ab14f42d205d48c1
William Dizon <wdchromium@gmail.com>
2016-03-07 15:27:25 -0700

    updating wily image with entrypoint

---
+++ ubuntu-wily/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eo pipefail
+
+if [ -z "$USER_PASSWORD" ]; then
+  echo >&2 'You need to specify USER_PASSWORD'
+  exit 1
+fi
+
+if [ "$USER_NAME" ]; then
+  # username specifically provided, will overwrite 'mc'
+  if [[ "$USER_NAME" =~ [^a-zA-Z0-9] ]]; then
+    echo >&2 'USER_NAME must contain only alphanumerics [a-zA-Z0-9]'
+    exit 1
+  fi
+else
+  echo >&2 'USER_NAME not provided; defaulting to "mc"'
+  USER_NAME=mc
+fi
+
+useradd -Ms /bin/false $USER_NAME
+echo "$USER_NAME:$USER_PASSWORD" | chpasswd
+echo >&2 "Created user: $USER_NAME"
+
+echo >&2 "Generating Self-Signed SSL..."
+sh /usr/games/minecraft/generate-sslcert.sh
+update-ca-certificates -f
+
+exec "$@"
