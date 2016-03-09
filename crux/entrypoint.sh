d832e339e375b765652d0ddbce082b2ba124a03a
William Dizon <wdchromium@gmail.com>
2016-03-09 14:04:34 -0700

    relocated crux java download to entrypoint.sh

--- crux/entrypoint.sh
+++ crux/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eo pipefail
 
+if [ "$ACCEPT_ORACLE_LICENSE" ]; then
+  echo >&2 'You need to ACCEPT_ORACLE_LICENSE=true in order to start this container.'
+  echo >&2 'Visit http://www.oracle.com/technetwork/java/javase/terms/license/index.html'
+  exit 1
+fi
+
 if [ -z "$USER_PASSWORD" ]; then
   echo >&2 'You need to specify USER_PASSWORD'
   exit 1
@@ -23,5 +29,15 @@ echo >&2 "Created user: $USER_NAME"
 
 echo >&2 "Generating Self-Signed SSL..."
 sh /usr/games/minecraft/generate-sslcert.sh
+
+#download and install oracle java
+mkdir /root/java
+cd /root/java
+wget -c --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u74-b02/server-jre-8u74-linux-x64.tar.gz
+tar -xf server-jre-8u74-linux-x64.tar.gz
+mv /root/java/jdk1.8.0_74 /usr/java
+cd /root
+rm -rf /root/java
+ln -s /usr/java/jre/bin/java /usr/bin/java
 
 exec "$@"
