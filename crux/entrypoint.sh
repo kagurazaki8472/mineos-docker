e7bccfb5baa49d761763e46b3350e39cac3b778f
William Dizon <wdchromium@gmail.com>
2016-03-10 15:46:47 -0700

    updated git on each entrypoint

--- crux/entrypoint.sh
+++ crux/entrypoint.sh
@@ -23,12 +23,24 @@ else
   USER_NAME=mc
 fi
 
-useradd -Ums /bin/false $USER_NAME
+if [ "$USER_UID" ]; then
+  # uid specifically provided, will overwrite 1000 default
+  if [[ "$USER_UID" =~ [^0-9] ]]; then
+    echo >&2 'USER_UID must contain only numerics [0-9]'
+    exit 1
+  fi
+else
+  USER_UID=1000
+fi
+
+useradd -Ums /bin/false -u $USER_UID $USER_NAME
 echo "$USER_NAME:$USER_PASSWORD" | chpasswd
 echo >&2 "Created user: $USER_NAME"
 
 echo >&2 "Generating Self-Signed SSL..."
 sh /usr/games/minecraft/generate-sslcert.sh
+
+cd /usr/games/minecraft && git fetch && git merge origin/master
 
 #download and install oracle java
 mkdir /root/java
