c6c714992e3a247c971cb84b270235c2e0b0e575
William Dizon <wdchromium@gmail.com>
2016-03-10 14:55:26 -0700

    added USER_UID option for volume file ownership

--- debian-jessie/entrypoint.sh
+++ debian-jessie/entrypoint.sh
@@ -17,9 +17,19 @@ else
   USER_NAME=mc
 fi
 
-useradd -Ms /bin/false $USER_NAME
+if [ "$USER_UID" ]; then
+  # uid specifically provided, will overwrite 1000 default
+  if [[ "$USER_UID" =~ [^0-9] ]]; then
+    echo >&2 'USER_UID must contain only numerics [0-9]'
+    exit 1
+  fi
+else
+  USER_UID=1000
+fi
+
+useradd -Ms /bin/false -u $USER_UID $USER_NAME
 echo "$USER_NAME:$USER_PASSWORD" | chpasswd
-echo >&2 "Created user: $USER_NAME"
+echo >&2 "Created user: $USER_NAME (uid: $USER_UID)"
 
 echo >&2 "Generating Self-Signed SSL..."
 sh /usr/games/minecraft/generate-sslcert.sh
